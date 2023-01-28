ac63986df386d16c9dfc92c8c6521e507921a94f
hugoherculano <hugohercullano@gmail.com>
2023-01-27 22:05:52 -0300

    typescript configuration, add dist

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-app.listen(8000, function () {
-    console.log("OLÁ, TURMA!");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 const app = express();
 
-app.listen(8000, () => {
-    console.log("OLÁ, WORLD!")
+app.get('/', (req, res) => {
+    return res.status(200).json({ success: 'OK' })
 })
+
+app.listen(8000, () => {
+    console.log("OLÁ, MUNDÃO!")
+});
